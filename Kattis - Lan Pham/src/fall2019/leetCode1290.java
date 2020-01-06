847bf015a1ffd0c6321f6cd85c6f4bb3e66efdb7
hoanglan21 <phaml@dickinson.edu>
2020-01-06 11:26:08 +0700

    try

--- Kattis - Lan Pham/src/fall2019/leetCode1290.java
+++ Kattis - Lan Pham/src/fall2019/leetCode1290.java
@@ -1,5 +1,39 @@
 package fall2019;
 
+import java.util.ArrayList;
+
 public class leetCode1290 {
 
+	public int getDecimalValue(ListNode head) {
+        ArrayList<Integer> values = new ArrayList<Integer>();
+        while (head.next != null) {
+            values.add(head.val);
+            head = head.next;
+        }
+        return helperMethod(values);
+        
+    }
+	
+	public int helperMethod(ArrayList<Integer> values) {
+		int summary = 0;
+		int cal = 0;
+		for (int index = values.size()-1; index >= 0; index--) {
+			if (values.get(index) == 1) {
+				summary += Math.pow(2, cal);
+				cal++;
+			}
+			else {
+				cal++;
+			}
+		}
+		return summary;
+	}
+	
+	public class ListNode {
+	      int val;
+	      ListNode next;
+	      ListNode(int x) { val = x; }
+	  }
 }
+
+	
